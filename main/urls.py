a090893e9c0d7816caf5085ae2004a3e72c3d698
oyatullohm <marziyahoni@gmail.com>
2024-07-29 07:01:19 +0500

    modall

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('teacher/',TeacherView.as_view(),name='teacher'),
     path('teacher/<int:pk>/',TeacherDetailView.as_view(),name='teacher_detail'),
     path('teacher-password/<int:pk>/',password,name='edit_password'),
+    path('add_teacher/',add_teacher,name='add_teacher'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.views import View
 from .models import *
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -22,6 +22,7 @@ class TeacherView(LoginRequiredMixin,View):
                     .prefetch_related('salaries', 'group_teachers','group_helpers')
         context = {
             'teacher':teacher,
+            'type':Teacher.TYPE
         }
         return render (request , 'teacher-all.html',context)
     
@@ -62,26 +63,31 @@ class TeacherDetailView(LoginRequiredMixin,View):
         user.is_active = active == 'on'
         user.save()
         group =  get_object_or_404(Group, id=group)
-        if int(user.type) <= 2:
-            if user.group_teachers is not None:
-                group = user.group_teachers
-                group.teacher = None
-                group.save()
+        if int(user.type) == 2:
+
+            if Group.objects.filter(teacher=user).exists():
+                group_teachers = user.group_teachers
+                group_teachers.teacher = None
+                group_teachers.save()
             group.teacher = user
             group.save()
         if int(user.type) == 3:
-            if user.group_helpers is not None:
-                group = user.group_helpers
-                group.helper = None
-                group.save()
+            if Group.objects.filter(helper=user).exists():
+                group_helpers = user.group_helpers
+                group_helpers.helper = None
+                group_helpers.save()
             group.helper = user
             group.save()
         return redirect (f'/teacher/{pk}/')
-
+@login_required
 def password(request,pk):
         password = request.POST.get('password')
         user = get_object_or_404(Teacher, id=pk)
         user.set_password(password)
         user.save()
         return redirect (f'/teacher/{pk}/')
+@login_required
+def add_teacher(request):
+    
+    return redirect ('/teacher')
         
