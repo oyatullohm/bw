178990c929104195a068551b142c25846197de0c
oyatullohm <marziyahoni@gmail.com>
2024-08-04 20:30:21 +0500

    f

--- config/urls.py
+++ config/urls.py
@@ -8,10 +8,10 @@ from django.contrib.auth.views import LogoutView
 from django.views.static import serve as mediaserve 
 
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
 
 
 
@@ -24,8 +24,8 @@ urlpatterns = [
     path('register/',RegisterView.as_view(),name='reset_password'),
     path('logout/', logout_, name='logout'),
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT )
 # urlpatterns += [
